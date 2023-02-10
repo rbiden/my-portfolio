6f89c802bbd085754a2434524225ade3914c0256
rbiden <rabiestrella@gmail.com>
2023-02-11 02:46:21 +0800

    Landing page

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./src/styles/main.css">
+    <link rel="stylesheet" href="https://kit.fontawesome.com/162af0fcdc.css" crossorigin="anonymous">
+    <title>Welcome to my Portfolio</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="nav-brand">
+                <h1>Hi there!</h1>
+                <span class="fa-solid fa-heart fa-xl"></span>
+            </div>
+            <div class="nav-items">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#">Background</a></li>
+                    <li><a href="#">Works</a></li>
+                    <li><a href="#">Contacts</a></li>
+                </ul>
+            </div>
+            <div class="nav-addons">
+                <button>Get Started!</button>
+            </div>  
+        </nav>
+    </header>
+    <div id="home">
+        <h1>Welcome to my <br><span>Personal Portfolio</span></h1>
+        <small>I'm grateful that you're here!</small>
+        <div class="floating-group">
+            <input type="text" required spellcheck="false">
+            <label>Browse my website</label>
+            <span class="fa-solid fa-magnifying-glass fa-xl"></span>
+        </div>
+    </div>
+
+    <script src="https://kit.fontawesome.com/162af0fcdc.js" crossorigin="anonymous"></script>
+    <script src="./scripts/index.js"></script>
+</body>
+</html>
